1ca1ac0f592e9310688d09df97eb9a60fe56aae4
unknown <prakasaadhi698@gmail.com>
2024-11-25 21:40:39 +0700

    add FAB button and static data in home screen

--- app/src/main/java/com/group2/papertrail/ui/home/HomeFragment.java
+++ app/src/main/java/com/group2/papertrail/ui/home/HomeFragment.java
@@ -9,23 +9,46 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.group2.papertrail.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Setup Recently Viewed RecyclerView
+        RecyclerView paperView = binding.reclyerViewRecentlyUsed;
+
+        paperView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false  ));
+
+        List<String> recentlyItems = Arrays.asList("item 1", "item 2", "item 3");
+
+        PaperRecylerViewAdapter recentlyAdapter = new PaperRecylerViewAdapter(recentlyItems);
+        paperView.setAdapter(recentlyAdapter);
+
+        // Setup Favorites RecyclerView
+        RecyclerView favoriteView = binding.recyclerViewFavorites;
+
+        favoriteView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        List<String> favoritesItems = Arrays.asList("item 1", "item 2", "item 3");
+
+        PaperRecylerViewAdapter favoriteAdapter = new PaperRecylerViewAdapter(favoritesItems);
+        favoriteView.setAdapter(favoriteAdapter);
+
+
         return root;
     }
 
---
+++ app/src/main/java/com/group2/papertrail/ui/home/PaperRecylerViewAdapter.java
@@ -0,0 +1,48 @@
+package com.group2.papertrail.ui.home;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.group2.papertrail.R;
+import java.util.List;
+
+public class PaperRecylerViewAdapter extends RecyclerView.Adapter<PaperRecylerViewAdapter.viewHolder> {
+    private List<String> items;
+
+    public PaperRecylerViewAdapter(List<String> items){
+        this.items = items;
+    }
+
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recently_viewed, parent, false);
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+        holder.titleTextView.setText(items.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    static class viewHolder extends RecyclerView.ViewHolder{
+        TextView titleTextView;
+
+        public viewHolder(@NonNull View itemView){
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.item_title);
+        }
+
+    }
+}
