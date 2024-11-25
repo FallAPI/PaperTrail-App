d5c351679c1a22b4c33be17547454172e9143047
Arajdian Altaf <arajdianaltaf123@gmail.com>
2024-11-25 17:42:12 +0700

    Add BaseDAO interface for future DAOs

---
+++ app/src/main/java/com/group2/papertrail/dao/BaseDAO.java
@@ -0,0 +1,11 @@
+package com.group2.papertrail.dao;
+
+import java.util.List;
+
+public interface BaseDAO<T> {
+    long insert(T model);
+    int update(T model);
+    int delete(T model);
+    T findById(long id);
+    List<T> findAll();
+}
