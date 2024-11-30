cb8fdb3009f144aec485966820ad460366ac9e31
Arajdian Altaf <arajdianaltaf123@gmail.com>
2024-11-30 12:02:37 +0700

    add missing list_item.xml

---
+++ app/src/main/java/com/group2/papertrail/ui/view/PDFPicker.java
@@ -0,0 +1,27 @@
+package com.group2.papertrail.ui.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class PDFPicker extends View {
+    public PDFPicker(Context context) {
+        super(context);
+    }
+
+    public PDFPicker(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PDFPicker(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public PDFPicker(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+}
