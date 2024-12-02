c8a6a17e2341dd7bf2dddd3013b7b0e99b0f0392
Arajdian Altaf <arajdianaltaf123@gmail.com>
2024-12-02 20:25:41 +0700

    fix: incorrect HomeViewModel instatiation

--- app/src/main/java/com/group2/papertrail/ui/home/HomeFragment.java
+++ app/src/main/java/com/group2/papertrail/ui/home/HomeFragment.java
@@ -5,38 +5,68 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.group2.papertrail.databinding.FragmentHomeBinding;
+import com.group2.papertrail.ui.PDFComponent;
+import com.group2.papertrail.ui.PDFComponentAdapter;
+import com.group2.papertrail.ui.PDFViewModel;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private HomeViewModel homeViewModel;
+    private PDFViewModel pdfViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        homeViewModel = new ViewModelProvider(this, new ViewModelProvider.Factory() {
+            @NonNull
+            @Override
+            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                return (T) new HomeViewModel(requireActivity().getApplication());
+            }
+        }).get(HomeViewModel.class);
+
+        pdfViewModel = new ViewModelProvider(this, new ViewModelProvider.Factory() {
+            @NonNull
+            @Override
+            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                return (T) new PDFViewModel(requireActivity().getApplication());
+            }
+        }).get(PDFViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        // Setup Recently Viewed RecyclerView
-        RecyclerView paperView = binding.reclyerViewRecentlyUsed;
 
-        paperView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false  ));
+        homeViewModel.getPdfIds().observe(getViewLifecycleOwner(), pdfIds -> {
+            pdfViewModel.loadPDF(pdfIds.toArray(new Long[0]), result -> {
+                if (Objects.requireNonNull(result) == PDFViewModel.PDFOperationResult.ERROR) {
+                    Toast.makeText(requireContext(), "Something went wrong when loading recently viewed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
 
-        List<String> recentlyItems = Arrays.asList("item 1", "item 2", "item 3");
+        binding.reclyerViewRecentlyUsed.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false  ));
 
-        PaperRecylerViewAdapter recentlyAdapter = new PaperRecylerViewAdapter(recentlyItems, getContext());
-        paperView.setAdapter(recentlyAdapter);
+        pdfViewModel.getPdfFiles().observe(getViewLifecycleOwner(), pdfs -> {
+            var pdfComponentList = pdfs.stream().map(PDFComponent::new).collect(Collectors.toList()); // same as return new PDFComponent(pdf)
+            var pdfAdapter = new PDFComponentAdapter(pdfComponentList);
+            binding.reclyerViewRecentlyUsed.setAdapter(pdfAdapter);
+        });
 
         // Setup Favorites RecyclerView
         RecyclerView favoriteView = binding.recyclerViewFavorites;
@@ -45,7 +75,7 @@ public class HomeFragment extends Fragment {
 
         List<String> favoritesItems = Arrays.asList("item 1", "item 2", "item 3");
 
-        PaperRecylerViewAdapter favoriteAdapter = new PaperRecylerViewAdapter(favoritesItems, getContext());
+        PaperRecylerViewAdapter favoriteAdapter = new PaperRecylerViewAdapter(favoritesItems, requireActivity().getApplicationContext());
         favoriteView.setAdapter(favoriteAdapter);
 
 
