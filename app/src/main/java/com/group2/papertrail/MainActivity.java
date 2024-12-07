252390f26b66efef83ae3bc32afad0b869197a86
Arajdian Altaf <arajdianaltaf123@gmail.com>
2024-12-08 01:31:29 +0700

    enable edge to edge color

--- app/src/main/java/com/group2/papertrail/MainActivity.java
+++ app/src/main/java/com/group2/papertrail/MainActivity.java
@@ -1,6 +1,7 @@
 package com.group2.papertrail;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.SparseBooleanArray;
 import android.view.Menu;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
+import androidx.core.view.WindowCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
